67f1a5c4d579ecdec54a77d455bfcf6d94cd5c8d
Betsegaw Lemma <betsegawlemma@gmail.com>
2019-10-21 16:10:26 +0300

    first commit

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseFiles("index.html"))
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
